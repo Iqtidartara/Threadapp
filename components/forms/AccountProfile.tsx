e37a0956b7ee2647e4042a0fab2c4525ae6d857c
iqtidartara <iqtidartara692@gmail.com>
2024-08-18 22:30:32 -0700

    implement

---
+++ components/forms/AccountProfile.tsx
@@ -0,0 +1,22 @@
+'use client'
+interface Props{
+    user : {
+        id: string;
+        objectId : string;
+        username : string;
+        name:string;
+        bio:string;
+        image : string;
+    };
+    btnTitle: string;
+}
+
+const AccountProfile = ({user,btnTitle}: Props) => {
+  return (
+   <form action="">
+    
+   </form>
+  )
+}
+
+export default AccountProfile;
